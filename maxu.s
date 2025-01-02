35d4de37c35191c6ad3f33380631a7c28530519f
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2025-01-02 14:25:26 +0000

    cleanup

--- maxu.s
+++ maxu.s
@@ -3,24 +3,24 @@ Array: .word 1, 2, 3
 
 .global _start
 _start:
-    ldr r0, =3
+    ldr r0, =3 
     ldr r1, =Array
     bl max
     b _start        // End of testing code
 
 // Return maximum element of unsigned array
 max:
-    sub r0, r0, #1
-    ldr r2, [r1, r0, lsl #2]
-    cmp r0, #0
-    moveq r0, r2
-    bxeq lr
-    sub r0, r0, #1
-_max:
-    ldr r3, [r1, r0, lsl #2]
-    cmp r3, r2
-    movhi r2, r3 // hi is for unsigned higher
+    ldr r2, [r1], #4
     subs r0, r0, #1
-    bge _max
+    beq _end
+_max:
+    ldr r3, [r1], #4
+    cmp r2, r3
+    movlo r2, r3
+    subs r0, r0, #1
+    bne _max
+_end:
     mov r0, r2
     bx lr
+	
+	
