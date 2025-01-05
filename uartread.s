03a7321ec980cd2b6390be18e33944b3f01f6fb6
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2025-01-05 16:26:01 +0000

    simplify code

--- uartread.s
+++ uartread.s
@@ -2,7 +2,7 @@
 Out: .skip 256	// Reserve some space for output
 
 .text
-//read_uart:	// This may be useful for debugging
+// read_uart:	// This may be useful for debugging
     ldr r0, =0xff201000
     ldr r0, [r0]
     bx lr
@@ -10,26 +10,25 @@ Out: .skip 256	// Reserve some space for output
 .global _start
 _start:
     ldr sp, =0x04000000	// Initialize SP for debugging.
-    ldr r0, =Out // Use Out as the output string
+    ldr r0, =Out		// Use Out as the output string
     bl uartgets
-1:  b 1b // Done
+1:  b 1b			// Done
 
 
 uartgets:
     push {lr}
     mov r1, r0
-    mov r12, #0
+    mov r12, r0
+    ldr r3, =0x0d000000
 poll:
     bl read_uart
-    and r2, r0, #255
-    ands r0, #0x8000
+    tst r0, #0x8000
     beq poll
-    strb r2, [r1], #1
-    add r12, #1
-    cmp r2, #0x0d
+    strb r0, [r1], #1
+    cmp r3, r0, lsl #24
     bne poll
     mov r2, #0
-    strb r2, [r1, #-1]
-    sub r0, r12, #1
+    strb r2, [r1, #-1]!
+    sub r0, r1, r12
     pop {pc}
 
