f6f95b308404b77351e0522d229c7954206c781f
SaiCharanMarrivada <92221348+SaiCharanMarrivada@users.noreply.github.com>
2025-01-13 05:04:58 +0530

    remove one recursive call.

--- treesize.s
+++ treesize.s
@@ -12,20 +12,24 @@ _start:
     1: b 1b
 
 .global size
-size:
-    push {lr}
+size: 
+    push {r2-r3,lr} 
+    mov r3, #0
     cmp r0, #0
-    popeq {pc}
-    push {r0}
-    ldr r0, [r0]
+    beq _end
+_size:
+    mov r2, r0
+    ldr r0, [r2]
     bl size
-    pop {r2}
-    push {r0}
+    add r3, r0, r3 
+    add r3, r3, #1
     ldr r0, [r2, #4]
-    bl size
-    pop {r3}
-    add r0, r3, r0
-    add r0, #1
-    pop {pc}
+    cmp r0, #0
+    bne _size
+_end:
+    mov r0, r3
+    pop {r2-r3, pc}
+ 
 
-
+	
+	
