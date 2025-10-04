ca45377bbedccf0b81d1c1b1477c5f5fff6759b3
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2025-10-04 07:11:27 +0000

    improve the version to use less instructions

--- hexstr.s
+++ hexstr.s
@@ -1,51 +1,46 @@
-// A test case to test your function with
-MyString: .skip 12    // Reserve some space to hold the output string
+MyString: .skip 12
 
 .global _start
 _start:
-    ldr r0, =MyString     // First parameter: Where to write
-    ldr r1, =0x0000000   // Second parameter: The number to process
+    ldr r0, =MyString
+    ldr r1, =0x0000000
     bl hexstr
-    b _start  // End of testing code
+    b _start
 
 .macro hex_to_char
-    ands r12, r1, #15
+    and r12, r1, #15
     ldrb r12, [r2, r12]
-    strb r12, [r0], #-1
+    strb r12, [r0, #-1]!
     lsr r1, r1, #4
 .endm
 
-// Convert number to string
 hexstr:
     cmp r1, #0
-    beq 0f // handle zero separately
-    clz r2, r1
-    rsb r2, r2, #32
-    lsr r3, r2, #2
-    cmp r2, r3, lsl #2
-    addne r3, r3, #1
+    beq handle_zero
+    clz r3, r1
+    rsb r3, r3, #35
+    lsr r3, r3, #2
     ldr r2, =lookup
     mov r12, #0
-    strb r12, [r0, r3] // null byte
+    strb r12, [r0, r3]
     add r0, r0, r3
-    subs r0, r0, #1
     ldr r12, =nibbles
     ldr pc, [r12, r3, lsl #2]
-    
-.irp n, 8, 7, 6, 5, 4, 3, 2, 1
-\n: 
-	hex_to_char
+
+.irp n, 8,7,6,5,4,3,2,1
+\n:
+    hex_to_char
 .endr
     bx lr
-0:
-    mov r12, #0
-    strb r12, [r0, #1]
-    mov r12, #48
+
+handle_zero:
+    strb r1, [r0, #1]
+    mov r12, #'0'
     strb r12, [r0]
     bx lr
 
 nibbles:
-    .word 0x0000 /* dummy */, 1b, 2b, 3b, 4b, 5b, 6b, 7b, 8b
+    .word 0x0000, 1b, 2b, 3b, 4b, 5b, 6b, 7b, 8b
 
 lookup:
     .ascii "0123456789abcdef"
